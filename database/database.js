1f4d3939c641483c3d26c77bf7b63fc7d56c36cb
João Pedro <joao.koguishi@gmail.com>
2023-11-30 17:17:03 -0300

    arquivo .env

--- database/database.js
+++ database/database.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 const Sequelize = require('sequelize');
-const connection = new Sequelize('dojo','root','04112003',{
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: "-03:00"
+
+const connection = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  timezone: process.env.DB_TIMEZONE
 });
 
 module.exports = connection;
